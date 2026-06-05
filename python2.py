c998c479f26e9d67753a4895405870aa3f918914
nmajufavour16 <nmajufavour16@gmail.com>
2026-06-05 11:27:06 +0100

    python day 3

--- python2.py
+++ python2.py
@@ -1,12 +1,12 @@
-var1 = "obi"
-print(var1)
-print(type(var1))
+# var1 = "obi"
+# print(var1)
+# print(type(var1))
 
-var2 = 25.5
-print(type(var2))
+# var2 = 25.5
+# print(type(var2))
 
-x = str(var2)
-print(type(x))
+# x = str(var2)
+# print(type(x))
 
 # var1 = "A"
 # var2 = "a"
@@ -24,33 +24,34 @@ print(type(x))
 # print(z)
 # print(x, y, z)
 
-me = "favour"
-for char in me:
-    print(char)
-    print(len(me))
+# me = "favour"
+# for char in me:
+#     print(char)
+#     print(len(me))
 
 sentence = "i'm learning python"
-print(len(sentence))
-print("python" in sentence)
-if "python" in sentence:
-    print("user is learning python")
-else:
-    print("user is not learning python")
-print(sentence[1:6])
-print(sentence[2:])
-print(sentence[:3])
+# print(len(sentence))
+# print("python" in sentence)
+# if "python" in sentence:
+#     print("user is learning python")
+# else:
+#     print("user is not learning python")
+# print(sentence[1:6])
+# print(sentence[2:])
+# print(sentence[:3])
 
 statement = "and it's been one week already"
 print(sentence + " " + statement)
+print(f"{sentence} {statement}")
 
-department = "information_technology"
-print(department[1:8:2])
-print(department[1::3])
+# department = "information_technology"
+# print(department[1:8:2])
+# print(department[1::3])
 
-bag = ["charger", "cord", "laptop", "book", "pen", "mouse", "phone"]
-print(bag[3])
+# bag = ["charger", "cord", "laptop", "book", "pen", "mouse", "phone"]
+# print(bag[3])
 
-name = " nmaju favour "
-print(name.upper())
-print(name.lower())
-print(name.strip())
+# name = " nmaju favour "
+# print(name.upper())
+# print(name.lower())
+# print(name.strip())
---
+++ python3.py
@@ -0,0 +1,34 @@
+# Python Day 3 - Boolean
+# print(bool("Welcome to Day 3!"))
+# print(bool(0))
+# print(bool(7))
+# print(bool(''))
+# print(bool('0'))
+# print(5==7)
+# print(5!=7)
+
+# Operators in Python
+b = 10
+b %= 3
+print(b)
+
+c = 12
+c %= 4
+print(c)
+
+# Number Conversion
+# 241, 65, 199, in binary
+# 208, 165 in hex 
+# '2D5A' hex to binary
+# 107 hex to decimal
+# 125 to oct
+# 11011101 to hex
+# 10101100 to oct
+
+x = y = 5
+print(x is y)
+
+print(b is not c)
+
+items = ["iPhone", "Laptop", "Charger", "Headphones"]
+print(type(items))
