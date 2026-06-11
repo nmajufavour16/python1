4edb79e8912ec90619f7fc93f0131156e29ab495
nmajufavour16 <nmajufavour16@gmail.com>
2026-06-11 13:34:16 +0100

    functions class

---
+++ functions/function2.py
@@ -0,0 +1,16 @@
+# The block of code inside a function must be indented.
+# A function can return data, or perform a task.
+
+def bia():
+    message = f"come here!!"
+    return message
+print(bia())
+
+# def birthday():
+#     age = +1
+#     return age
+
+def add(a, b):
+    return a + b
+result = add(88, 56)
+print(result)
---
+++ june11/syntax.py
@@ -0,0 +1,35 @@
+# for x in range(4):
+#     print("#", end = "")
+    
+# for x in range(1):
+#     print("#", end = " ")
+#     for y in range(4):
+#         print("#")
+        
+# names = ["favour", "uche", "nmaju", "phayvo"]
+# for name in names:
+#     print(name[1])
+    
+for x in range(4):
+    for y in range(4 - x):
+        print("#", end = " ")
+    print()
+    
+    # 
+    # # 
+    # # # 
+    # # # # 
+    
+for z in range(4):
+    for a in range(2+ z):
+        print("#", end = " ")
+    print()
+    
+    # # # # 
+    # # # 
+    # # 
+    # 
+    # 
+    # # 
+    # # # 
+    # # # # 
